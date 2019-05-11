c9bb079fc45e449cc1ec6794feec51afee454513
Carl NOMBO <carl.nombo@gmail.com>
2019-05-11 20:13:14 +0000

    -modification menu, utilisation d'un dropdown -options d'ajouts droite, gauche, fils au menu -Dans le fichier javascript ajout des fonctions pour droite gauche qui fonctionent et fils qu'il faut contrôler après un l'ajout d'un fils *************problèmes*************** -lors de l'ajout d'un élément, tous les propriétés censées appartenir à cet élément ne marche pas (principalement le dropdown qui doit sortir) -Lors du clique sur un élément, le dropdown ne sors pas au bon endroit -Lors du clique sur le fils ajouté, le menu affiché est lié à son parent et non à lui même -Quand fait l'action indiquée sur le précédent point, l'ajout beugue y compris pour droite et gauche

--- diagram/diagram.js
+++ diagram/diagram.js
@@ -1,3 +1,25 @@
-/*$(".element").on("click",function() {
-	$("#menu").fadeIn();
-});*/
+$(".element").on("click",function() {
+	//$("#menu").fadeIn();
+	var e=this;
+	$("#menu1").fadeIn();
+
+	$("#droite").on("click",function(){
+		var nominantion=prompt("nommination : ");
+		$(e).after("<li class='element'><a class='btn btn-outline-info'  href='#' >"+nominantion+"</a></li>");
+		});
+	
+
+	$("#gauche").on("click",function(){
+		var nominantion=prompt("nommination : ");
+		$(e).before("<li class='element'><a class='btn btn-outline-info'  href='#' >"+nominantion+"</a></li>");
+		});
+
+	$("#fils").on("click",function(){
+		var nominantion=prompt("nommination : ");
+		$(e).append("<ul><li class='element'><a class='btn btn-outline-info'  href='#' >"+nominantion+"</a></li></ul>");
+		});
+
+	$("#close").on("click",function(){$("#menu1").fadeOut();});
+});
+
+//$().button('toggle')
